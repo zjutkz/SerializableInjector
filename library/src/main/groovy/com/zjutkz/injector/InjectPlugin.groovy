c6390d13f42426b72343f3897be459bcd4b01a74
亢哲 <kangzhe@weidian.com>
2017-11-17 18:30:53 +0800

    remove isApp judgement

--- library/src/main/groovy/com/zjutkz/injector/InjectPlugin.groovy
+++ library/src/main/groovy/com/zjutkz/injector/InjectPlugin.groovy
@@ -1,7 +1,6 @@
 package com.zjutkz.injector
 
 import com.android.build.gradle.AppExtension
-import com.android.build.gradle.AppPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 /**
@@ -12,12 +11,9 @@ public class InjectPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        def isApp = project.plugins.hasPlugin(AppPlugin)
-        if (isApp) {
-            project.extensions.create("injectorConfig",InjectorExtension)
-            def android = project.extensions.findByType(AppExtension)
-            android.registerTransform(new InjectTransform(project))
-        }
+        project.extensions.create("injectorConfig",InjectorExtension)
+        def android = project.extensions.findByType(AppExtension)
+        android.registerTransform(new InjectTransform(project))
     }
 }
 
