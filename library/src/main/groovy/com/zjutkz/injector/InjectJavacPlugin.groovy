20cb5781850615b61a37cd063c955ddfd22f3823
亢哲 <kangzhe@weidian.com>
2017-11-20 22:30:29 +0800

    afterEvaluate

--- library/src/main/groovy/com/zjutkz/injector/InjectJavacPlugin.groovy
+++ library/src/main/groovy/com/zjutkz/injector/InjectJavacPlugin.groovy
@@ -16,39 +16,41 @@ public class InjectJavacPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
-        project.extensions.create("injectorConfig",InjectorExtension.class)
+        project.afterEvaluate {
+            project.extensions.create("injectorConfig",InjectorExtension.class)
 
-        def variants
-        if (project.plugins.hasPlugin(AppPlugin)) {
-            AppExtension android = project.extensions.getByType(AppExtension.class)
-            variants = android.getApplicationVariants()
-        } else {
-            LibraryExtension android = project.extensions.getByType(LibraryExtension.class)
-            variants = android.getLibraryVariants()
-        }
+            def variants
+            if (project.plugins.hasPlugin(AppPlugin)) {
+                AppExtension android = project.extensions.getByType(AppExtension.class)
+                variants = android.getApplicationVariants()
+            } else {
+                LibraryExtension android = project.extensions.getByType(LibraryExtension.class)
+                variants = android.getLibraryVariants()
+            }
 
-        def sdkDir
-        Properties properties = new Properties()
-        File local = project.rootProject.file('local.properties')
-        if(local.exists()){
-            properties.load(local.newDataInputStream())
-        }
-        if (System.getenv("ANDROID_HOME") != null) {
-            sdkDir = System.getenv("ANDROID_HOME")
-        } else {
-            sdkDir = properties.getProperty('sdk.dir')
-        }
-        def androidJar = "${sdkDir}/platforms/${project.android.compileSdkVersion}/android.jar"
-        println("android sdk version: " + project.android.compileSdkVersion)
-        Injector.injectPath(androidJar)
+            def sdkDir
+            Properties properties = new Properties()
+            File local = project.rootProject.file('local.properties')
+            if(local.exists()){
+                properties.load(local.newDataInputStream())
+            }
+            if (System.getenv("ANDROID_HOME") != null) {
+                sdkDir = System.getenv("ANDROID_HOME")
+            } else {
+                sdkDir = properties.getProperty('sdk.dir')
+            }
+            def androidJar = "${sdkDir}/platforms/${project.android.compileSdkVersion}/android.jar"
+            println("android sdk version: " + project.android.compileSdkVersion)
+            Injector.injectPath(androidJar)
 
-        variants.all { variant ->
-            JavaCompile javaCompile = (JavaCompile) (variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile)
-            javaCompile.doLast {
-                InjectorExtension injectorExtension = project.getExtensions().findByType(InjectorExtension.class)
-                println("packageName: " + injectorExtension.packageName)
-                println("packagePattern: " + injectorExtension.packagePattern)
-                Injector.injectDir(javaCompile.getDestinationDir().toString(),injectorExtension.packageName,injectorExtension.packagePattern)
+            variants.all { variant ->
+                JavaCompile javaCompile = (JavaCompile) (variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile)
+                javaCompile.doLast {
+                    InjectorExtension injectorExtension = project.getExtensions().findByType(InjectorExtension.class)
+                    println("packageName: " + injectorExtension.packageName)
+                    println("packagePattern: " + injectorExtension.packagePattern)
+                    Injector.injectDir(javaCompile.getDestinationDir().toString(),injectorExtension.packageName,injectorExtension.packagePattern)
+                }
             }
         }
     }
